de99c7589dfdc149f7bf63af1b3fe6917db2d751
unknown <urke.1998ds@gmail.com>
2025-04-15 14:46:10 +0200

    exposing handlers

--- DataTables.ServerSideProcessing.EFCore/QueryBuilder.cs
+++ DataTables.ServerSideProcessing.EFCore/QueryBuilder.cs
@@ -1,8 +1,7 @@
 ﻿using DataTables.ServerSideProcessing.Data.Models;
+using DataTables.ServerSideProcessing.EFCore.Filtering;
+using DataTables.ServerSideProcessing.EFCore.Sorting;
 using Microsoft.EntityFrameworkCore;
-using static DataTables.ServerSideProcessing.EFCore.Filtering.ColumnFilterHandler;
-using static DataTables.ServerSideProcessing.EFCore.Filtering.GenericFilterHandler;
-using static DataTables.ServerSideProcessing.EFCore.Sorting.SortHandler;
 
 namespace DataTables.ServerSideProcessing.EFCore;
 public static class QueryBuilder
@@ -12,6 +11,21 @@ public static class QueryBuilder
         return query.HandleGenericFilter(properties, search)
                     .HandleColumnFilters(filters)
                     .HandleSorting(sortOrder);
+    }
+
+    public static IQueryable<T> HandleGenericFilter<T>(this IQueryable<T> query, IEnumerable<string>? properties = null, string? search = null) where T : class
+    {
+        return GenericFilterHandler.HandleGenericFilter(query, properties, search);
+    }
+
+    public static IQueryable<T> HandleColumnFilters<T>(this IQueryable<T> query, IEnumerable<DataTableFilterBaseModel>? filters = null) where T : class
+    {
+        return ColumnFilterHandler.HandleColumnFilters(query, filters);
+    }
+
+    public static IQueryable<T> HandleSorting<T>(this IQueryable<T> query, IEnumerable<SortModel>? sortOrder = null) where T : class
+    {
+        return SortHandler.HandleSorting(query, sortOrder);
     }
 
     public async static Task<List<T>> ExecuteQuery<T>(this IQueryable<T> query, int skip, int pageSize, CancellationToken ct) where T : class
