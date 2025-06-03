c5cbe1ecf4a7bf722f9c469b22829e7f09b981d2
Uros Milic <urke.1998ds@gmail.com>
2025-06-03 16:55:08 +0200

    resposne builder added, request parser changed

--- DataTables.ServerSideProcessing.Utils/Helpers.cs
+++ DataTables.ServerSideProcessing.Utils/Helpers.cs
@@ -1,7 +1,14 @@
-﻿namespace DataTables.ServerSideProcessing.Utils;
+﻿using Microsoft.Extensions.Primitives;
+
+namespace DataTables.ServerSideProcessing.Utils;
 internal static class Helpers
 {
     internal static int ToInt(this string broj)
+    {
+        return int.TryParse(broj, out int result) ? result : -1;
+    }
+
+    internal static int ToInt(this StringValues broj)
     {
         return int.TryParse(broj, out int result) ? result : -1;
     }
