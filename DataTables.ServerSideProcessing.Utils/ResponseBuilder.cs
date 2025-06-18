074458e28a52e63a3522b5e3c8703744f1c36f3f
Uros Milic <urke.1998ds@gmail.com>
2025-06-18 11:33:28 +0200

    add sync version for response builder

--- DataTables.ServerSideProcessing.Utils/ResponseBuilder.cs
+++ DataTables.ServerSideProcessing.Utils/ResponseBuilder.cs
@@ -48,4 +48,46 @@ public class ResponseBuilder
         response.Data = await dataFunc(request);
         return response;
     }
+
+    /// <summary>
+    /// Builds a <see cref="DataTableResponse{TViewModel}"/> synchronously based on the provided request form and data retrieval functions.
+    /// </summary>
+    /// <typeparam name="TViewModel">The type of the data items to be returned in the response.</typeparam>
+    /// <param name="requestForm">The form collection containing DataTables request parameters.</param>
+    /// <param name="totalCountFunc">A function that asynchronously returns the total count of records (before filtering).</param>
+    /// <param name="filteredCountFunc">A function that asynchronously returns the count of records after filtering, given a search string.</param>
+    /// <param name="dataFunc">A function that asynchronously returns the data items for the current page, based on the parsed request.</param>
+    /// <param name="parseSort">Indicates whether to parse sorting information from the request form. Default is true.</param>
+    /// <param name="parseFilters">Indicates whether to parse filter information from the request form. Default is true.</param>
+    /// <returns>
+    /// A <see cref="DataTableResponse{TViewModel}"/> containing the requested data and metadata.
+    /// </returns>
+    public static DataTableResponse<TViewModel> BuildDataTableResponse<TViewModel>(
+        IFormCollection requestForm,
+        Func<int> totalCountFunc,
+        Func<string?, int> filteredCountFunc,
+        Func<DataTableRequest, List<TViewModel>> dataFunc,
+        bool parseSort = true,
+        bool parseFilters = true)
+    {
+        if (requestForm == null || requestForm.Count == 0)
+            return new DataTableResponse<TViewModel>();
+
+        var totalCount = totalCountFunc();
+        if (totalCount == 0)
+            return new DataTableResponse<TViewModel>() { Draw = requestForm["draw"] };
+
+        var response = new DataTableResponse<TViewModel>() { Draw = requestForm["draw"], RecordsTotal = totalCount };
+
+        var request = RequestParser.ParseRequest(requestForm, parseSort, parseFilters);
+
+        var filteredCount = filteredCountFunc(request.Search);
+        if (filteredCount == 0)
+            return response;
+
+        response.RecordsFiltered = filteredCount;
+
+        response.Data = dataFunc(request);
+        return response;
+    }
 }
